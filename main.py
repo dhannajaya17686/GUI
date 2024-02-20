97d3f5924f36b34643f9eaf16543cf1ab759f901
dananjayamudalige@gmail <dananjayamudalige@gmail.com>
2024-02-20 22:48:28 +0530

    NO Stuuf doesnt change even in dark mdoe yay !

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'untitledMygwYX.ui'
+## Form generated from reading UI file 'untitledEdhWWM.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.6.2
 ##
@@ -18,18 +18,20 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QPushButton,
     QSizePolicy, QWidget)
 
-class Ui_MainWindow(QWidget):
+
+class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(676, 684)
-        MainWindow.setStyleSheet(u"background-color: rgb(0,0,0);")
+        MainWindow.resize(676, 694)
+        MainWindow.setStyleSheet(u"")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setStyleSheet(u"background-color: rgb(0, 0, 0);")
         self.UiMainIcon = QLabel(self.centralwidget)
         self.UiMainIcon.setObjectName(u"UiMainIcon")
         self.UiMainIcon.setGeometry(QRect(0, 10, 61, 41))
-        self.UiMainIcon.setStyleSheet(u"image: url(smile.svg);")
+        self.UiMainIcon.setStyleSheet(u"image: url(:/newPrefix/projects/GUI/smile.svg);")
         self.widget = QWidget(self.centralwidget)
         self.widget.setObjectName(u"widget")
         self.widget.setGeometry(QRect(380, 10, 281, 51))
@@ -38,7 +40,7 @@ class Ui_MainWindow(QWidget):
         self.UiMainIcon_2 = QLabel(self.widget)
         self.UiMainIcon_2.setObjectName(u"UiMainIcon_2")
         self.UiMainIcon_2.setGeometry(QRect(0, 10, 61, 41))
-        self.UiMainIcon_2.setStyleSheet(u"image: url(account.png);")
+        self.UiMainIcon_2.setStyleSheet(u"image: url(:/newPrefix/account.png);")
         self.label = QLabel(self.widget)
         self.label.setObjectName(u"label")
         self.label.setGeometry(QRect(60, 10, 211, 31))
@@ -46,8 +48,9 @@ class Ui_MainWindow(QWidget):
 "font: 700 18pt \"Noto Sans\";")
         self.label_2 = QLabel(self.centralwidget)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(0, 40, 311, 61))
+        self.label_2.setGeometry(QRect(0, 50, 311, 61))
         self.label_2.setStyleSheet(u"font: 700 23pt \"Noto Sans\";\n"
+"color: rgb(255, 255, 255);\n"
 "padding:10px;")
         self.widget_2 = QWidget(self.centralwidget)
         self.widget_2.setObjectName(u"widget_2")
@@ -56,13 +59,15 @@ class Ui_MainWindow(QWidget):
         self.label_3 = QLabel(self.centralwidget)
         self.label_3.setObjectName(u"label_3")
         self.label_3.setGeometry(QRect(0, 100, 311, 61))
-        self.label_3.setStyleSheet(u"font: 700 17pt \"Noto Sans\";\n"
+        self.label_3.setStyleSheet(u"color:rgb(255, 255, 255);\n"
+"font: 700 17pt \"Noto Sans\";\n"
 "padding:10px;")
         self.label_4 = QLabel(self.centralwidget)
         self.label_4.setObjectName(u"label_4")
         self.label_4.setGeometry(QRect(0, 340, 311, 61))
         self.label_4.setStyleSheet(u"font: 700 17pt \"Noto Sans\";\n"
-"padding:10px;")
+"padding:10px;\n"
+"color: rgb(255, 255, 255);")
         self.pushButton = QPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
         self.pushButton.setGeometry(QRect(20, 390, 241, 201))
@@ -73,7 +78,8 @@ class Ui_MainWindow(QWidget):
         self.label_5.setObjectName(u"label_5")
         self.label_5.setGeometry(QRect(390, 100, 311, 61))
         self.label_5.setStyleSheet(u"font: 700 17pt \"Noto Sans\";\n"
-"padding:10px;")
+"padding:10px;\n"
+"color:rgb(255, 255, 255);")
         self.pushButton_2 = QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName(u"pushButton_2")
         self.pushButton_2.setGeometry(QRect(410, 150, 241, 181))
@@ -84,7 +90,8 @@ class Ui_MainWindow(QWidget):
         self.label_6.setObjectName(u"label_6")
         self.label_6.setGeometry(QRect(390, 340, 311, 61))
         self.label_6.setStyleSheet(u"font: 700 17pt \"Noto Sans\";\n"
-"padding:10px;")
+"padding:10px;\n"
+"color: rgb(255, 255, 255);")
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
